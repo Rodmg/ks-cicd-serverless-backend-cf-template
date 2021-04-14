345fc66624a27a9e37dcff9c4fd6cbd9a526b84a
Rodrigo Mendez Gamboa <rodrigo.mendez@infolob.com>
2021-04-14 15:43:19 -0500

    Added default repo

--- cloudformation/src/config.js
+++ cloudformation/src/config.js
@@ -12,6 +12,9 @@ const config = {
   s3DeletionPolicy: "Retain",
   apiUrl: "api.example.com",
   appUrl: "example.com",
+  gitSource: "GITHUB",
+  gitLocation:
+    "https://github.com/ksquareincmx/ks-cicd-serverless-backend-cf-template",
 };
 
 module.exports = {
